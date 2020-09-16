575b0c0bd7d986adf7e0c27cedced8039ae613b6
Anwesh43 <anweshthecool0@gmail.com>
2020-09-16 14:27:39 +0530

    adding BMNode class

--- index.ts
+++ index.ts
@@ -135,3 +135,45 @@ class Animator {
         }
     }
 }
+
+class BMNode {
+
+    state : State = new State()
+    prev : BMNode 
+    next : BMNode 
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BMNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBMNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BMNode {
+        var curr : BMNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
